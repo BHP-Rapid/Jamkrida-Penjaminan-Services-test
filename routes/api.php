8e5241d7221885a34b05d500c71ab3f4e7968214
Ahmad Faisal <ahmadfaisall.020597@gmail.com>
2026-04-10 14:09:40 +0700

    checkpoint custom bond

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\CustomBondServices\CustomBondTransactionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PenjaminanTransactionController;
 
 Route::get('/penjaminan/penjaminan-data', [PenjaminanTransactionController::class, 'index']);
 Route::get('/penjaminan/detail-additional-document', [PenjaminanTransactionController::class, 'getAdditionalDocProduct']);
 Route::get('/penjaminan/detail-certified-permohonan',[PenjaminanTransactionController::class, 'GetDetailCertificateByID']);
+
+Route::get('/v2/penjaminan/penjaminan-custom-bond-byid', [CustomBondTransactionController::class, 'show']);
+Route::post('/v2/penjaminan/custom-bond/create', [CustomBondTransactionController::class, 'store']);
+Route::post('/v2/penjaminan/custom-bond/update-draft/{trxNo}', [CustomBondTransactionController::class, 'updateDraft']);
