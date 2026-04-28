492b9e6d3b6e1d81e96d9286bc80dcec986d3674
Doricky dMorenTh <49485962+dmorenth22@users.noreply.github.com>
2026-04-28 17:24:44 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,23 @@ use App\Http\Controllers\KonstruksiServices\KonstruksiTransactionController;
 use App\Http\Controllers\KreditMikroKecilServices\KreditMikroKecilController;
 use App\Http\Controllers\KreditUsahaServices\KreditUsahaController;
 use App\Http\Controllers\KURServices\KURTransactionController;
-use App\Http\Controllers\MultigunaController;
 use App\Http\Controllers\MultigunaServices\MultigunaController as MultigunaServicesMultigunaController;
 use App\Http\Controllers\PaymentGatewayController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PenjaminanTransactionController;
 use App\Http\Controllers\SuretyBondTransactionServices\SuretyBondTransactionController;
 
-Route::get('/penjaminan/penjaminan-data', [PenjaminanTransactionController::class, 'index']);
-Route::get('/penjaminan/detail-additional-document', [PenjaminanTransactionController::class, 'getAdditionalDocProduct']);
-Route::get('/penjaminan/detail-certified-permohonan', [PenjaminanTransactionController::class, 'GetDetailCertificateByID']);
+Route::prefix('/v2/penjaminan')->group(function () {
+    Route::get('/penjaminan-data', [PenjaminanTransactionController::class, 'index']);
+    Route::get('/detail-additional-document', [PenjaminanTransactionController::class, 'getAdditionalDocProduct']);
+    Route::get('/detail-certified-permohonan', [PenjaminanTransactionController::class, 'GetDetailCertificateByID']);
+});
 
-Route::post('/penjaminan/payment-gateway', [PaymentGatewayController::class, 'generatePaymentGateway']);
-Route::post('/penjaminan/cancel-payment', [PaymentGatewayController::class, 'cancelPaymentMidtrans']);
-Route::post('/penjaminan/renew-payment-token', [PaymentGatewayController::class, 'RenewPaymentGateway']);
-Route::post('/penjaminan/validate-payment', [PaymentGatewayController::class, 'CheckPaymentMidtrans']);
+Route::prefix('/v2/payment-gateway')->group(function () {
+    Route::post('/create-transaction', [PaymentGatewayController::class, 'createTransaction']);
+    Route::post('/update-transaction/{id}', [PaymentGatewayController::class, 'updateTransaction']);
+    Route::get('/transaction-status/{id}', [PaymentGatewayController::class, 'getTransactionStatus']);
+});
 
 // PENJAMINAN MULTIGUNA
 Route::prefix('/v2/penjaminan/multiguna')->group(function () {
@@ -45,30 +47,37 @@ Route::prefix('/v2/penjaminan/ajp')->group(function () {
     Route::get('/debt', [AjpController::class, 'createTrxDebitur']);
 });
 
+Route::prefix('/v2/penjaminan/custom-bond')->group(function () {
+    Route::get('/penjaminan-custom-bond-byid', [CustomBondTransactionController::class, 'show']);
+    Route::post('/create', [CustomBondTransactionController::class, 'store']);
+    Route::post('/update-draft/{trxNo}', [CustomBondTransactionController::class, 'updateDraft']);
+    Route::post('/approved-penjaminan', [CustomBondTransactionController::class, 'ApprovePenjaminanCSTB']);
+    Route::post('/upload-bukti-bayar-manual', [CustomBondTransactionController::class, 'uploadPembayaranManual']);
+    Route::post('/submit-draft/{trxNo}', [CustomBondTransactionController::class, 'submitDraft']);
+    Route::get('/detail-payment-custom-bond', [CustomBondTransactionController::class, 'GetDetailPaymentCstb']);
+});
 
-Route::get('/v2/penjaminan/penjaminan-custom-bond-byid', [CustomBondTransactionController::class, 'show']);
-Route::post('/v2/penjaminan/custom-bond/create', [CustomBondTransactionController::class, 'store']);
-Route::post('/v2/penjaminan/custom-bond/update-draft/{trxNo}', [CustomBondTransactionController::class, 'updateDraft']);
-Route::post('/v2/penjaminan/custom-bond/approved-penjaminan', [CustomBondTransactionController::class, 'ApprovePenjaminanCSTB']);
-Route::post('/v2/penjaminan/custom-bond/upload-bukti-bayar-manual', [CustomBondTransactionController::class, 'uploadPembayaranManual']);
-Route::post('/v2/penjaminan/custom-bond/submit-draft/{trxNo}', [CustomBondTransactionController::class, 'submitDraft']);
-Route::get('/v2/penjaminan/detail-payment-custom-bond', [CustomBondTransactionController::class, 'GetDetailPaymentCstb']);
+// PENJAMINAN SURETY BOND
+Route::prefix('/v2/penjaminan/surety-bond')->group(function () {
+    Route::get('/detail', [SuretyBondTransactionController::class, 'show']);
+    Route::post('/create', [SuretyBondTransactionController::class, 'store']);
+    Route::post('/update-draft/{trxNo}', [SuretyBondTransactionController::class, 'update']);
+    Route::post('/submit-draft/{trxNo}', [SuretyBondTransactionController::class, 'submitDraft']);
+    Route::post('/approved-penjaminan', [SuretyBondTransactionController::class, 'approvePenjaminanSB']);
+    Route::get('/detail-payment', [SuretyBondTransactionController::class, 'getDetailPaymentSrtb']);
+    Route::post('/upload-bukti-bayar-manual', [SuretyBondTransactionController::class, 'uploadPembayaranManual']);
+    Route::get('/detail-payment-surety-bond', [SuretyBondTransactionController::class, 'getDetailPaymentSrtb']);
+});
 
-Route::get('/v2/penjaminan/surety-bond/detail', [SuretyBondTransactionController::class, 'show']);
-Route::post('/v2/penjaminan/surety-bond/create', [SuretyBondTransactionController::class, 'store']);
-Route::post('/v2/penjaminan/surety-bond/update-draft/{trxNo}', [SuretyBondTransactionController::class, 'update']);
-Route::post('/v2/penjaminan/surety-bond/submit-draft/{trxNo}', [SuretyBondTransactionController::class, 'submitDraft']);
-Route::post('/v2/penjaminan/surety-bond/approved-penjaminan', [SuretyBondTransactionController::class, 'approvePenjaminannSB']);
-Route::get('/v2/penjaminan/detail-payment-surety-bond', [SuretyBondTransactionController::class, 'getDetailPaymentSrtb']);
-Route::post('/v2/penjaminan/surety-bond/upload-bukti-bayar-manual', [SuretyBondTransactionController::class, 'uploadPembayaranManual']);
-
-Route::post('/v2/penjaminan/kredit-mikro-kecil/create', [KreditMikroKecilController::class, 'store']);
-Route::post('/v2/penjaminan/kredit-mikro-kecil/approve-penjaminan', [KreditMikroKecilController::class, 'ApprovePenjaminanKMK']);
-Route::get('/v2/penjaminan/template-base/download-template', [KreditMikroKecilController::class, 'DownloadTemplateKMK']);
-Route::put('/v2/penjaminan/kredit-mikro-kecil/update-draft/{trxNo}', [KreditMikroKecilController::class, 'updateDraft']);
-Route::get('/v2/penjaminan/kredit-mikro-kecil/detail-full-kmk', [KreditMikroKecilController::class, 'GetDetailPaymentKMK']);
-Route::get('/v2/penjaminan/kredit-mikro-kecil/detail-installment-kmk-list', [KreditMikroKecilController::class, 'GetDetailListPaymentKMK']);
-Route::post('/v2/penjaminan/kredit-mikro-kecil/upload-bukti-bayar-manual', [KreditMikroKecilController::class, 'UploadPembayaranManualKMK']);
+Route::prefix('/v2/penjaminan/kredit-mikro-kecil')->group(function () {
+    Route::post('/v2/penjaminan/kredit-mikro-kecil/create', [KreditMikroKecilController::class, 'store']);
+    Route::post('/v2/penjaminan/kredit-mikro-kecil/approve-penjaminan', [KreditMikroKecilController::class, 'ApprovePenjaminanKMK']);
+    Route::get('/v2/penjaminan/template-base/download-template', [KreditMikroKecilController::class, 'DownloadTemplateKMK']);
+    Route::put('/v2/penjaminan/kredit-mikro-kecil/update-draft/{trxNo}', [KreditMikroKecilController::class, 'updateDraft']);
+    Route::get('/v2/penjaminan/kredit-mikro-kecil/detail-full-kmk', [KreditMikroKecilController::class, 'GetDetailPaymentKMK']);
+    Route::get('/v2/penjaminan/kredit-mikro-kecil/detail-installment-kmk-list', [KreditMikroKecilController::class, 'GetDetailListPaymentKMK']);
+    Route::post('/v2/penjaminan/kredit-mikro-kecil/upload-bukti-bayar-manual', [KreditMikroKecilController::class, 'UploadPembayaranManualKMK']);
+});
 
 // Route::get('/v2/penjaminan/kredit-usaha-rakyat/detail/{id}', [KURTransactionController::class, 'show']);
 Route::prefix('/v2/penjaminan/kredit-usaha-rakyat')->group(function () {
